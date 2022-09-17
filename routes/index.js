5b7b02c7e46719e21a6f621ae0f0ccd1928e0a0d
jack.hai <jack.hai@ibaiqiu.com>
2022-09-17 17:21:38 +0800

    提交钉钉样式调整-4

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: jack.hai
  * @Date: 2022-08-27 12:15:55
- * @LastEditTime: 2022-09-17 17:06:38
+ * @LastEditTime: 2022-09-17 17:21:13
  * @Description: 
  */
 var express = require('express');
@@ -45,13 +45,27 @@ router.post('/receiveHooks', async function  (req, res, next) {
   }
   
   // https://www.blog.fugen.fun/
-  let str =
-    '> 项目名称：' + obj.project.toUpperCase() + '  '+
-    '> 当前环境：' + obj.env + '  ' +
-    '> 错误级别：' + '<font color=red>'+ obj.level + '</font>' + '  ' +
-    '> 详情地址：' +'[点击查看](' + obj.url + ')' +'' + '<br>' +
-    '> 错误内容：' +'<font color=red>'+ obj.value + '</font>' + '  ' +
-    '> 项目请求路径：' + '[点击查看](' + obj.request + ')' + ''
+  // let str =
+  //   '> 项目名称：' + obj.project.toUpperCase() + '  '+
+  //   '> 当前环境：' + obj.env + '  ' +
+  //   '> 错误级别：' + '<font color=red>'+ obj.level + '</font>' + '  ' +
+  //   '> 详情地址：' +'[点击查看](' + obj.url + ')' +'' + '<br>' +
+  //   '> 错误内容：' +'<font color=red>'+ obj.value + '</font>' + '  ' +
+  //   '> 项目请求路径：' + '[点击查看](' + obj.request + ')' + ''
+  
+  // console.log(str)
+
+  let str = `> 项目名称：${obj.project.toUpperCase()}
+
+> 当前环境：${obj.env}
+  
+> 错误级别：<font color=red>${obj.level}</font>
+
+> 详情地址：[点击查看](${obj.url})
+
+> 错误内容：<font color=red>${obj.value}</font>
+
+> 项目请求路径：[点击查看](${obj.request})`
   let params = {
     msgtype: "markdown",
     markdown: {
